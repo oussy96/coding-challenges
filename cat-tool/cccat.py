168870abc99d616b6d9c6bbb887063642fb279e5
Oussy Doucoure <oussy@Ubuntu-22.04.3>
2024-03-28 11:48:00 +0100

    Step 2 completed - Reads input from standard in

--- cat-tool/cccat.py
+++ cat-tool/cccat.py
@@ -1,12 +1,46 @@
 import os
+import argparse
 import sys
 
-def get_filename():
-    if len(sys.argv) != 2:
-        print("Usage: cccat <filename>")
-        sys.exit(1)
+
+def set_parse_commands():
+    # Create ArgumentParser object
+    parser = argparse.ArgumentParser(prog='cccat', description='Process cat file.')
+
+    # Create mutually exclusive group for filename and input
+    group = parser.add_mutually_exclusive_group()
+
+    # Add arguments
+    group.add_argument('filename', type=str, nargs='?'
+                       , help='The name of file to display his content')
+    group.add_argument('-r', '--read', action='store_true'
+                       , help='Read the input from standard in')
     
-    return sys.argv[1]
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    return args
+
+
+def get_parse_filename(args):
+    return args.filename
+
+
+def get_parse_input():
+    return sys.stdin
+
+
+def get_filepath(filename):
+    return os.path.join(os.getcwd(), filename)
+
+
+def cat_argument(args):
+    if args.filename:
+        file_arg = get_parse_filename(args)
+        cat_file(get_filepath(file_arg))
+    elif args.read:
+        stdin_ = get_parse_input()
+        cat_input(stdin_)
 
 
 def cat_file(filename):
@@ -23,8 +57,13 @@ def cat_file(filename):
         print(f"Error: {filename} not found")
 
 
+def cat_input(stdin_):
+    for line in stdin_:
+        print(line.strip())
+
+
 if __name__ == "__main__":
     # Build the files path
-    filepath = os.path.join(os.getcwd(), get_filename())
-    
-    cat_file(filepath)
+    args = set_parse_commands()
+    cat_argument(args)
+
